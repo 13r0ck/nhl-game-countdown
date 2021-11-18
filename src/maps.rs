1f2056cf4f0d66740fab537b95bef03d258de262
Brock <brock@pop-os.localdomain>
2021-11-18 13:54:26 -0700

    :sparkles: add POC for google cloudbuild file for autodeployment of updates

--- src/maps.rs
+++ src/maps.rs
@@ -1,9 +1,9 @@
 use phf::phf_map;
 
 pub static ICONS: phf::Map<&'static str, usize> = phf_map! {
-    "21" => 47478,
+    "21" => 47478, // Colorado Avalanche
 };
 
 pub static MSG: phf::Map<&'static str, &'static str> = phf_map! {
-    "21" => "GO AVS GO",
+    "21" => "GO AVS GO", // Colorado Avalanche
 };
