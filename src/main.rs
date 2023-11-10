4f94dee82aadfa0760c85f2aceb46266ffde454e
Brock Szuszczewicz <brock@szu.email>
2023-11-10 22:54:37 +0000

    Handle API request errors properly. Don't use `unwrap`
    Resets to clock on api change, rather than internal server error.

--- src/main.rs
+++ src/main.rs
@@ -19,33 +19,35 @@ async fn api(team: &'_ str, utc_offset: &'_ str, format: &'_ str) -> Json<LaMetr
     let now_local = Local::now();
     let now_user = DateTime::from_local(now_local.naive_local(), Offset::new(utc_offset).into());
     let in_99_days = now_local + Duration::days(99);
-    match reqwest::get(format!(
+    let request = reqwest::get(format!(
         "https://statsapi.web.nhl.com/api/v1/schedule?teamId={}&startDate={}&endDate={}",
         team,
         simple_date(now_local),
         simple_date(in_99_days)
     ))
-    .await
-    .unwrap()
-    .json::<NhlApi>()
-    .await
-    {
-        Ok(nhl_api) => {
-            let now_utc = DateTime::<Utc>::from_utc(now_local.naive_utc(), Utc);
-            if let Some((game_time, state)) = nhl_api.current_or_next_game(now_utc) {
-                if state.is_active() {
-                    Json(LaMetricIndicator::new(format!("{}", state), team.icon()))
+    .await;
+
+    if let Ok(req) = request {
+        match req.json::<NhlApi>().await {
+            Ok(nhl_api) => {
+                let now_utc = DateTime::<Utc>::from_utc(now_local.naive_utc(), Utc);
+                if let Some((game_time, state)) = nhl_api.current_or_next_game(now_utc) {
+                    if state.is_active() {
+                        Json(LaMetricIndicator::new(format!("{}", state), team.icon()))
+                    } else {
+                        Json(LaMetricIndicator::new(
+                            pretty_timer(now_utc.timestamp(), game_time),
+                            team.icon(),
+                        ))
+                    }
                 } else {
-                    Json(LaMetricIndicator::new(
-                        pretty_timer(now_utc.timestamp(), game_time),
-                        team.icon(),
-                    ))
+                    error(now_user, team, use_24_hour)
                 }
-            } else {
-                error(now_user, team, use_24_hour)
             }
+            Err(_) => error(now_user, team, use_24_hour),
         }
-        Err(_) => error(now_user, team, use_24_hour),
+    } else {
+        error(now_user, team, use_24_hour)
     }
 }
 
